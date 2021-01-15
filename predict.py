4d0ffa57037082948afac6f2e34b4d8c5817b85b
Daniel Campos <spacemanidol@gmail.com>
2021-01-15 15:37:42 -0600

    adding basic code from https://github.com/sgrvinod/a-PyTorch-Tutorial-to-Image-Captioning

---
+++ predict.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+import json
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import skimage.transform
+import argparse
+from scipy.misc import imread, imresize
+from PIL import Image
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Show, Attend, and Tell - Tutorial - Generate Caption')
+
+    parser.add_argument('--img', '-i', help='path to image')
+    parser.add_argument('--model', '-m', help='path to model')
+    parser.add_argument('--word_map', '-wm', help='path to word map JSON')
+    parser.add_argument('--beam_size', '-b', default=5, type=int, help='beam size for beam search')
+    parser.add_argument('--dont_smooth', dest='smooth', action='store_false', help='do not smooth alpha overlay')
+
+    args = parser.parse_args()
+
+    # Load model
+    checkpoint = torch.load(args.model, map_location=str(device))
+    decoder = checkpoint['decoder']
+    decoder = decoder.to(device)
+    decoder.eval()
+    encoder = checkpoint['encoder']
+    encoder = encoder.to(device)
+    encoder.eval()
+
+    # Load word map (word2ix)
+    with open(args.word_map, 'r') as j:
+        word_map = json.load(j)
+    rev_word_map = {v: k for k, v in word_map.items()}  # ix2word
+
+    # Encode, decode with attention and beam search
+    seq, alphas = caption_image_beam_search(encoder, decoder, args.img, word_map, args.beam_size)
+    alphas = torch.FloatTensor(alphas)
+
+    # Visualize caption and attention of best sequence
+    visualize_att(args.img, seq, alphas, rev_word_map, args.smooth)
