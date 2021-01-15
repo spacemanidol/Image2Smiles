85b9cb49f440ac4cffad8ef1b102e156dde245d1
Daniel Campos <spacemanidol@gmail.com>
2021-01-15 13:31:47 -0600

    process to take smiles and make image

---
+++ create_images_from_simles.py
@@ -0,0 +1,32 @@
+import os
+import argparse
+from rdkit import Chem
+from rdkit.Chem import Draw
+import deepchem as dc
+"""
+m = Chem.MolFromMolFile('data/input.mol')
+Chem.MolToSmiles(m,isomericSmiles=False)
+consider adding diversity to training data if its an issue
+https://www.rdkit.org/docs/GettingStartedInPython.html
+featurizer = dc.feat.graph_features.ConvMolFeaturizer()
+mol_object = featurizer.featurize(mols=molecules)
+"""
+
+def main(args):
+    with open(args.input_file,'r') as f:
+        with open(os.path.join(args.output_folder,args.input_file)) as w:
+            for l in f:
+                l = l.strip()
+                m = Chem.MolFromSmiles(l)
+                if m != None:
+                    Draw.MolToFile(m,os.path.join(args.output_folder, '{}.png'.format(l)))
+                    w.write("{}\n".format(l))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', type=str, default='data/validation.smi', help='source of input smiles data')
+    parser.add_argument('--output_folder', type=str, default='data/validation_images')
+    parser.add_argument('--smiles', action='store_true', help='diversify images produces')
+    parser.add_argument('--diversify', action='store_true', help='diversify images produces')
+    args = parser.parse_args()
